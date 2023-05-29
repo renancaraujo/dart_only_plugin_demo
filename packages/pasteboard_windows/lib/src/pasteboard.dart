2e414bb131af72b169e43539ab3cd4d435ee48ca
renan <renanmatheuscorreiaaraujo@gmail.com>
2023-05-29 22:02:27 +0100

    omg

--- packages/pasteboard_windows/lib/src/pasteboard.dart
+++ packages/pasteboard_windows/lib/src/pasteboard.dart
@@ -1,9 +1,8 @@
 import 'dart:ffi';
-import 'dart:js_interop';
 import 'package:ffi/ffi.dart';
 import 'package:win32/win32.dart';
 
-void main() {
+void main2() {
   if (OpenClipboard(NULL) != 0) {
     if (IsClipboardFormatAvailable(CF_TEXT) != 0) {
       final hData = GetClipboardData(CF_TEXT);
@@ -21,19 +20,22 @@ void main() {
   }
 }
 
-void main2() {
+void main() {
   final user32 = DynamicLibrary.open('user32.dll');
 
   final textToCopy = 'Hello, clipboard!';
+
 
   final hGlobal = GlobalAlloc(
     0x0002 /* GMEM_MOVEABLE */,
     textToCopy.length * 2 + 2,
   );
-  final lpData = GlobalLock(hGlobal);
+  final lpData = GlobalLock(hGlobal).cast<Utf16>();
   if (lpData == nullptr) {
     return;
   }
+
+  lpData.setString(textToCopy);
 
 
   GlobalUnlock(hGlobal);
